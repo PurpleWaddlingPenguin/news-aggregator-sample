d505ccc74bd27939b86fc427241edb63d9265e54
PurpleWaddlingPenguin <purple@waddlingpenguin.local>
2024-04-16 13:58:56 +1000

    feat(news-aggregator): Initial work completed for a News Aggregator with `The Guardian` as an example.

---
+++ src/news-api/NewsSnippet.cs
@@ -0,0 +1,11 @@
+﻿namespace news_api
+{
+    public class NewsSnippet
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string? SummaryText { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
